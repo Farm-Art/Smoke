feb847f004e5f38036c96e26238cc5892935e40f
Farm-Art <farmart@protonmail.com>
2019-03-14 19:10:09 +0300

    bugfixes, improved code readability

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ from forms import *
 from sqlalchemy.exc import IntegrityError
 
 
+def check_for_logged_in():
+    return User.query.get(session.get('user_id', 0))
+
+
+def check_for_permission(user, item_id, item_type):
+    if user:
+        item = item_type.query.get(item_id)
+        if item:
+            return item
+        return 404
+    return 403
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    logged_in = 'username' in session
+    user = check_for_logged_in()
     return render_template('index.html', title='Smoke - Main',
-                           logged_in=logged_in)
+                           logged_in=user)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -69,21 +82,21 @@ def logout():
 
 @app.route('/catalog')
 def catalog():
-    logged_in = 'username' in session
-    if logged_in:
-        acc_type = User.query.get(session['user_id']).account_type
+    user = check_for_logged_in()
+    if user:
+        acc_type = user.account_type
     else:
         acc_type = 'def'
     products = Software.query.all()
     return render_template('catalog.html', title='Smoke - Catalog',
                            products=products,
-                           logged_in=logged_in,
+                           logged_in=user,
                            account_type=acc_type)
 
 
 @app.route('/product/<int:id>')
 def product_page(id):
-    logged_in = 'username' in session
+    user = check_for_logged_in()
     product = Software.query.get(id)
     if product:
         reviews = product.reviews
@@ -92,7 +105,7 @@ def product_page(id):
                                title='Smoke - {}'.format(product.title),
                                session=session,
                                news=news,
-                               logged_in=logged_in,
+                               logged_in=user,
                                product=product,
                                reviews=reviews)
     else:
@@ -101,230 +114,214 @@ def product_page(id):
 
 @app.route('/product/<int:id>/news')
 def all_news(id):
-    logged_in = 'username' in session
-    if logged_in:
-        is_developer = Software.query.get(id).user_id == session['user_id']
-    else:
-        is_developer = False
-    if Software.query.get(id):
+    user = check_for_logged_in()
+    software = Software.query.get(id)
+    if software:
         return render_template('news.html',
                                product=Software.query.get(id),
                                news=Software.query.get(id).news,
-                               is_developer=is_developer,
-                               logged_in=logged_in)
+                               is_developer=software.user_id == user.id,
+                               logged_in=user)
     else:
         return abort(404)
 
 
 @app.route('/product/<int:product_id>/news/<int:news_id>')
 def news(product_id, news_id):
-    logged_in = 'username' in session
-    if logged_in:
-        user_id = session['user_id']
-        is_developer = Software.query.get(product_id).user_id == user_id
+    user = check_for_logged_in()
+    software = Software.query.get(product_id)
+    if user:
+        is_developer = software.user_id == user.id
     else:
-        user_id = -1
         is_developer = False
-    if Software.query.get(product_id):
+    software = Software.query.get(product_id)
+    if software:
         news = News.query.get(news_id)
         if news:
             return render_template('onenews.html',
-                                   product=Software.query.get(product_id),
-                                   news=News.query.get(news_id),
+                                   product=software,
+                                   news=news,
                                    is_developer=is_developer,
-                                   logged_in=logged_in,
-                                   user_id=user_id,
+                                   logged_in=user,
+                                   user_id=user.id,
                                    comments=news.comments)
 
 
 @app.route('/product/<int:product_id>/news/add_news', methods=['GET', 'POST'])
 def add_news(product_id):
     form = AddNewsForm()
-    product = Software.query.get(product_id)
-    if form.validate_on_submit():
-        news = News(software_id=product_id,
-                    title=form.title.data,
-                    body=form.body.data)
-        db.session.add(news)
-        db.session.commit()
-        return redirect(url_for('all_news', id=product_id))
-    if product:
-        logged_in = 'username' in session
-        if logged_in:
-            user = User.query.get(session['user_id'])
-            if user.id == product.user_id:
-                return render_template('add_news.html', title='Smoke - Add News',
-                                       logged_in=logged_in,
-                                       form=form)
-            return abort(403)
-        return abort(403)
-    return abort(404)
+    user = check_for_logged_in()
+    product = check_for_permission(user, product_id, Software)
+    if isinstance(product, int):
+        return abort(product)
+    if product.user_id == user.id:
+        if form.validate_on_submit():
+            news = News(software_id=product_id,
+                        title=form.title.data,
+                        body=form.body.data)
+            db.session.add(news)
+            db.session.commit()
+            return redirect(url_for('all_news', id=product_id))
+        return render_template('add_news.html', title='Smoke - Add News',
+                                logged_in=user, form=form)
 
 
 @app.route('/add_software', methods=['GET', 'POST'])
 def add_software():
-    logged_in = 'username' in session
-    if logged_in:
-        user = User.query.get(session['user_id'])
-        form = AddSoftwareForm()
-        if form.validate_on_submit():
-            errors = []
-            if len(form.title.data) > 100:
-                errors.append('Title is too long! Max length - 100 symbols')
-            if len(form.description.data) > 1000:
-                errors.append('Description is too long! Max length - 1000 symbols')
-            if errors:
+    form = AddSoftwareForm()
+    user = check_for_logged_in()
+    if user:
+        if user.account_type == 'dev':
+            if form.validate_on_submit():
+                errors = []
+                if len(form.title.data) > 100:
+                    errors.append('Title is too long! Max length - 100 symbols')
+                if len(form.description.data) > 1000:
+                    errors.append('Description is too long! Max length - 1000 symbols')
+                if errors:
+                    return render_template('add_software.html',
+                                           title='Smoke - Add Software',
+                                           logged_in=user,
+                                           form=form,
+                                           errors=errors)
+                software = Software(user_id=user.id,
+                                    title=form.title.data,
+                                    description=form.description.data,
+                                    link=form.link.data)
+                screenshots = form.screenshots.data.split('\n')
+                for screenshot in screenshots:
+                    caption, description, link = screenshot.split(';')
+                    software.screenshots.append(Screenshot(link=link,
+                                                           caption=caption,
+                                                           description=description))
+                    db.session.add(software)
+                    db.session.commit()
+                    return redirect(url_for('product_page', id=software.id))
                 return render_template('add_software.html',
                                        title='Smoke - Add Software',
-                                       logged_in=logged_in,
-                                       form=form,
-                                       errors=errors)
-            software = Software(user_id=user.id,
-                                title=form.title.data,
-                                description=form.description.data,
-                                link=form.link.data)
-            screenshots = form.screenshots.data.split('\n')
-            for screenshot in screenshots:
-                caption, description, link = screenshot.split(';')
-                software.screenshots.append(Screenshot(link=link,
-                                                       caption=caption,
-                                                       description=description))
-            db.session.add(software)
-            db.session.commit()
-            return redirect(url_for('product_page', id=software.id))
-        return render_template('add_software.html',
-                               title='Smoke - Add Software',
-                               logged_in=logged_in,
-                               form=form)
+                                       logged_in=user,
+                                       form=form)
+            return render_template('add_software.html',
+                                   title='Smoke - Add Software',
+                                   logged_in=user,
+                                   form=form)
+        return abort(403)
     return abort(403)
 
 
 @app.route('/product/<int:id>/add_review', methods=['GET', 'POST'])
 def add_review(id):
     form = AddReviewForm()
-    logged_in = 'username' in session
-    if logged_in:
-        user = User.query.get(session['user_id'])
-        software = Software.query.get(id)
-        if software:
-            if software in user.softwares:
-                return abort(403)
-            if form.validate_on_submit():
-                if len(form.body.data) > 1000:
-                    return render_template('add_review.html',
-                                           title='Smoke - Add Review',
-                                           form=form,
-                                           logged_in=logged_in,
-                                           errors=[
-                                               'Your review is too long! Max length - 1000'])
-                review = Review(rating=form.rating.data,
-                                body=form.body.data,
-                                user_id=user.id,
-                                software_id=id)
-                db.session.add(review)
-                db.session.commit()
-                return redirect(url_for('product_page', id=id))
-            return render_template('add_review.html',
-                                   title='Smoke - Add Review',
-                                   form=form,
-                                   logged_in=logged_in)
-        return abort(404)
+    user = check_for_logged_in()
+    software = check_for_permission(user, id, Software)
+    if isinstance(software, int):
+        return abort(software)
+    if software.user_id != user.id:
+        if form.validate_on_submit():
+            if len(form.body.data) > 1000:
+                return render_template('add_review.html',
+                                       title='Smoke - Add Review',
+                                       form=form,
+                                       logged_in=user,
+                                       errors=[
+                                           'Your review is too long! Max length - 1000'])
+            review = Review(rating=form.rating.data,
+                            body=form.body.data,
+                            user_id=user.id,
+                            software_id=id)
+            db.session.add(review)
+            db.session.commit()
+            return redirect(url_for('product_page', id=id))
+        return render_template('add_review.html',
+                               title='Smoke - Add Review',
+                               form=form,
+                               logged_in=user)
     return abort(403)
 
 
 @app.route('/product/<int:product_id>/news/<int:news_id>/add_comment', methods=['GET', 'POST'])
 def add_comment(product_id, news_id):
     form = AddCommentForm()
-    logged_in = 'username' in session
-    if logged_in:
-        user = User.query.get(session['user_id'])
-        news = News.query.get(news_id)
-        if news:
-            if form.validate_on_submit():
-                if len(form.body.data) > 1000:
-                    return render_template('add_comment.html',
-                                           title='Smoke - Add Comment',
-                                           form=form,
-                                           logged_in=logged_in,
-                                           errors=[
-                                               'Your comment is too long! Max length - 1000'])
-                comment = Comment(body=form.body.data,
-                                  user_id=user.id,
-                                  news_id=news.id)
-                db.session.add(comment)
-                db.session.commit()
-                return redirect(url_for('news', product_id=product_id, news_id=news_id))
+    user = check_for_logged_in()
+    news = check_for_permission(user, news_id, News)
+    if isinstance(news, int):
+        return abort(news)
+    if form.validate_on_submit():
+        if len(form.body.data) > 1000:
             return render_template('add_comment.html',
                                    title='Smoke - Add Comment',
                                    form=form,
-                                   logged_in=logged_in)
-        return abort(404)
-    return abort(403)
+                                   logged_in=user,
+                                   errors=[
+                                       'Your comment is too long! Max length - 1000'])
+        comment = Comment(body=form.body.data,
+                          user_id=user.id,
+                          news_id=news.id)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('news', product_id=product_id, news_id=news_id))
+    return render_template('add_comment.html',
+                           title='Smoke - Add Comment',
+                           form=form,
+                           logged_in=user)
 
 
 @app.route('/delete_comment/<int:id>')
 def delete_comment(id):
-    comment = Comment.query.get(id)
-    if comment:
-        logged_in = 'username' in session
-        if logged_in:
-            user = User.query.get(session['user_id'])
-            news = comment.news
-            if user.id == comment.user_id:
-                db.session.delete(comment)
-                db.session.commit()
-                return redirect(url_for('news', product_id=news.software_id, news_id=news.id))
-            return abort(403)
-        return abort(403)
-    return abort(404)
+    user = check_for_logged_in()
+    comment = check_for_permission(user, id, Comment)
+    if isinstance(comment, int):
+        return abort(comment)
+    if comment.user_id == user.id:
+        news = comment.news
+        db.session.delete(comment)
+        db.session.commit()
+        return redirect(url_for('news', product_id=news.software_id, news_id=news.id))
+    return abort(403)
 
 
 @app.route('/delete_news/<int:id>')
 def delete_news(id):
-    news = News.query.get(id)
-    if news:
-        product_id = news.software_id
-        logged_in = 'username' in session
-        if logged_in:
-            user = User.query.get(session['user_id'])
-            if news in user.news:
-                db.session.delete(news)
-                db.session.commit()
-                return redirect(url_for('all_news', id=product_id))
-            return abort(403)
-        return abort(403)
-    return abort(404)
+    user = check_for_logged_in()
+    news = check_for_permission(user, id, News)
+    if isinstance(news, int):
+        return abort(news)
+    if news.user_id == user.id:
+        for comment in news.comments:
+            db.session.delete(comment)
+        db.session.delete(news)
+        db.session.commit()
+        return redirect(url_for('all_news', id=news.software_id))
+    return abort(403)
 
 
 @app.route('/delete_software/<int:id>')
 def delete_software(id):
-    software = Software.query.get(id)
-    if software:
-        logged_in = 'username' in session
-        if logged_in:
-            user = User.query.get(session['user_id'])
-            if software in user.softwares:
-                for screenshot in software.screenshots:
-                    db.session.delete(screenshot)
-                for review in software.reviews:
-                    db.session.delete(review)
-                for news in software.news:
-                    db.session.delete(news)
-                db.session.delete(software)
-                db.session.commit()
-                return redirect(url_for('catalog'))
-            return abort(403)
-        return abort(403)
-    return abort(404)
+    user = check_for_logged_in()
+    software = check_for_permission(user, id, Software)
+    if isinstance(software, int):
+        return abort(software)
+    if software.user_id == user.id:
+        for screenshot in software.screenshots:
+            db.session.delete(screenshot)
+        for review in software.reviews:
+            db.session.delete(review)
+        for news in software.news:
+            for comment in news.comments:
+                db.session.delete(comment)
+            db.session.delete(news)
+        db.session.delete(software)
+        db.session.commit()
+        return redirect(url_for('catalog'))
+    return abort(403)
 
 
 @app.route('/delete_review/<int:id>')
 def delete_review(id):
     review = Review.query.get(id)
     if review:
-        logged_in = 'username' in session
-        if logged_in:
-            user = User.query.get(session['user_id'])
+        user = check_for_logged_in()
+        if user:
             if review in user.reviews:
                 product_id = review.software_id
                 db.session.delete(review)
@@ -337,16 +334,16 @@ def delete_review(id):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    logged_in = 'username' in session
+    user = check_for_logged_in()
     return render_template('not_found.html', title='Smoke - 404',
-                           logged_in=logged_in)
+                           logged_in=user)
 
 
 @app.errorhandler(403)
 def not_allowed(error):
-    logged_in = 'username' in session
+    user = check_for_logged_in()
     return render_template('not_allowed.html', title='Smoke - 403',
-                           logged_in=logged_in)
+                           logged_in=user)
 
 @app.route('/403')
 def not_allowed_check():
